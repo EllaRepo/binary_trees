1eb18dd4b712f543866feaa06c528f01a217296a
EllaRepo <eliaberango@gmail.com>
2023-08-03 13:58:18 -0400

    Fix indentation

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - deletes an entire binary tree
+ * binary_tree_delete - deletes an entire binary tree
  * @tree: a pointer to the root node of the tree to delete
  *
  * Return: None
